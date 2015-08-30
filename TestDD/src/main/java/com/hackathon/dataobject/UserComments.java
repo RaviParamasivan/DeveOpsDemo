3bb3478ea6524c8cff021cd7d3d28f81a49a8faa
RaviParamasivan <ravi.paramasivam.m@gmail.com>
2015-08-30 13:03:48 -0700

    Update UserComments.java

--- TestDD/src/main/java/com/hackathon/dataobject/UserComments.java
+++ TestDD/src/main/java/com/hackathon/dataobject/UserComments.java
@@ -1,77 +1,86 @@
-package com.hackathon.dataobject;
+package com.hackathon.util;
 
-public class UserComments {
-	
-	
-	private String comments = "";
-	private String title = "";
-	private String categaroy = "";
-	private String moduleName = "";
-	private boolean isSensitive = false;
-	private String userMailId = "";
-	private String devGroupId = "";
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
-	public String getComments() {
-		return comments;
+
+import com.hackathon.dataobject.UserComments; 
+
+
+
+public class DataSource {
+	Connection connect = null;
+	public Connection readDataBase() throws Exception {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connect = DriverManager.getConnection("jdbc:mysql://us-cdbr-iron-east-02.cleardb.net/ad_561f50e17a446b4?user=b8019806ec6494&password=3e315020");
+		} catch (Exception e) {
+			throw e;
+		}
+		return connect;
 	}
 
-	public void setComments(String comments) {
-		this.comments = comments;
+	public String getModuleName() throws Exception {	
+		String options="";
+		ResultSet resultSet = null;	
+		Statement statement = null;
+		try {
+			connect=readDataBase();
+			System.out.println("connect   "+connect);
+			statement = connect.createStatement();
+			resultSet = statement.executeQuery("select * FROM jira_module");			
+			while (resultSet.next()) {
+				options+="<option value='"+resultSet.getString("mail_id")+"' >"+resultSet.getString("module_name")+"</option>";
+			}	
+		}catch(Exception e){
+			e.printStackTrace();
+			throw e;
+		}finally{
+			if (resultSet != null) {
+				resultSet.close();
+			}
+			if (statement != null) {
+				statement.close();
+			}
+			if (connect != null) {
+				connect.close();
+			}
+		}
+		return options;
 	}
 
-	public String getTitle() {
-		return title;
+	public void updateCustomeComments(UserComments userComments) throws Exception {
+		Connection connect = null; 
+		PreparedStatement preparedStatement = null; 
+		try {
+			
+			connect=readDataBase();
+			System.out.println("connect          >"+connect);
+			System.out.println("userComments          >"+userComments);
+			preparedStatement = connect.prepareStatement("INSERT INTO customer_commands(title,commands,category,module_name,issensitive,user_mail_id,dev_group_id,created_date) values (?, ?, ?, ?, ? , ?, ?, ?)"); 
+			preparedStatement.setString(1, userComments.getTitle()); 
+			preparedStatement.setString(2, userComments.getComments()); 
+			preparedStatement.setString(3, userComments.getCategaroy()); 
+			preparedStatement.setString(4, userComments.getModuleName()); 
+			preparedStatement.setString(5, userComments.isSensitive()==true?"Y":"N"); 
+			preparedStatement.setString(6, userComments.getUserMailId()); 
+			preparedStatement.setString(7, userComments.getDevGroupId()); 
+			preparedStatement.setDate(8, new Date(0)); 
+			preparedStatement.executeUpdate();			
+		}catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Exception while updating the module:  "+e);
+		}finally{
+			if (preparedStatement != null) {
+				preparedStatement.close();
+			}
+			if (connect != null) {
+				connect.close();
+			}			
+		}
 	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getCategaroy() {
-		return categaroy;
-	}
-
-	public void setCategaroy(String categaroy) {
-		this.categaroy = categaroy;
-	}
-
-	public String getModuleName() {
-		return moduleName;
-	}
-
-	public void setModuleName(String moduleName) {
-		this.moduleName = moduleName;
-	}
-
-	public boolean isSensitive() {
-		return isSensitive;
-	}
-
-	public void setSensitive(boolean isSensitive) {
-		this.isSensitive = isSensitive;
-	}
-
-	public String getUserMailId() {
-		return userMailId;
-	}
-
-	public void setUserMailId(String userMailId) {
-		this.userMailId = userMailId;
-	}
-
-	public String getDevGroupId() {
-		return devGroupId;
-	}
-
-	public void setDevGroupId(String devGroupId) {
-		this.devGroupId = devGroupId;
-	}
-
-	@Override
-	public String toString() {
-		return "UserComments [comments=" + comments + ", title=" + title + ", categaroy=" + categaroy + ", moduleName="
-				+ moduleName + ", isSensitive=" + isSensitive + ", userMailId=" + userMailId + ", devGroupId="
-				+ devGroupId + "]";
-	}
-
 }
