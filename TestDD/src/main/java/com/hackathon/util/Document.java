8c7b8c90433ac4009f0e87fcd1b39998bfd36a6d
RaviParamasivan <ravi.paramasivam.m@gmail.com>
2015-08-30 15:16:52 -0700

    Create Document.java

---
+++ TestDD/src/main/java/com/hackathon/util/Document.java
@@ -0,0 +1,16 @@
+
+package com.hackathon.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Document {
+
+	public Map<String, Integer> tokens;
+
+	public String category;
+
+	public Document() {
+		tokens = new HashMap<String, Integer>();
+	}
+}
